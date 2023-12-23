912164b524ba69a50b24d24c1e7e1643b08e6f04
wuwenyu <johnnywwy@gmail.com>
2023-12-23 17:41:55 +0800

    feat：删除无效代码

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
+
+// 引入 ant-design-vue 组件库
+import Antd from 'ant-design-vue';
+
+// 引入组件库样式表文件
+import 'ant-design-vue/dist/reset.css';
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Antd)
+  .mount('#app')
