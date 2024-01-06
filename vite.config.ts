fbe7c1a0b712d751dbf3777f4191c0f1e477df6c
wuwenyu <johnnywwy@gmail.com>
2024-01-06 10:35:46 +0800

    feat：引入tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueJsx from "@vitejs/plugin-vue-jsx";
 
 //  引入 uno.css
-import UnoCSS from 'unocss/vite'
-import { presetUno, presetAttributify, presetIcons } from 'unocss'
+import UnoCSS from "unocss/vite";
+import { presetUno, presetAttributify, presetIcons } from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      "@": "/src",
+    },
   },
   plugins: [
     vue(),
+    VueJsx(),
     UnoCSS({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons(),
-      ],
+      configFile: "uno.config.ts",
     }),
   ],
-})
+});
